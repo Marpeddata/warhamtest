156cffbec90a4affcad58d9dd3550b9286a78843
Mark <sutsko@gmail.com>
2023-06-21 21:38:06 +0200

    spring boot quick start complete

--- src/main/java/com/test/test/rest/testController.java
+++ src/main/java/com/test/test/rest/testController.java
@@ -1,10 +1,17 @@
 package com.test.test.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class testController {
+
+    //Property injection from application.properties file
+    @Value("${datasheet.name}")
+    private String datasheetName;
+    @Value("${faction.name}")
+    private String factionName;
 
 
     @GetMapping("/")
@@ -22,6 +29,11 @@ public class testController {
     public String winner(){
         return "winner winner chicken dinner!";
 
+    }
+
+    @GetMapping("/datasheet")
+    public String datasheet(){
+        return datasheetName +  " - " + factionName;
     }
 
 
